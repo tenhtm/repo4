83b1e82562f8c20ac4a9a0ec0b8ecb12fcc72eb3
GD <18339957376@163.com>
2018-10-28 21:20:12 +0800

    把本地工程添加到git

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -6,4 +6,9 @@ public class FirstClass {
         System.out.println("1111111111");
         System.out.println("1111111111");
     }
+
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
 }
